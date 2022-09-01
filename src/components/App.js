d7f47c2d82f8ac092650f9a1f882a13581a418b3
Pete Fowler <fowler.pete@gmail.com>
2022-09-01 15:30:26 -0600

    Complete exercise

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,27 @@ import HogBox from './HogBox';
 import hogs from "../porkers_data";
 
 function App() {
-	const [ hogsShown, setHogsShown ] = useState(hogs);
 	const [ showGreased, setShowGreased ] = useState(false);
-	const [ sortedByName, setSortedByName ] = useState(false)
-	const [ sortedByWeight, setSortedByWeight ] = useState(false)
+	const [ sortedBy, setSortedBy ] = useState(null)
 
 	function sortName() {
-		if(sortedByName === false) {
-			setHogsShown(hogsShown => hogsShown.sort((a, b) => {
+		setSortedBy(sortedBy => 'name');
+	}
+	
+	function sortWeight() {
+		setSortedBy(sortedBy => 'weight');
+	}
+
+	function toggleGreased() {
+		setShowGreased(showGreased => !showGreased);
+	}
+
+const hogsShown = hogs
+	.filter(hog => showGreased ? hog.greased : hog)
+	.sort((a, b) => {
+		if(sortedBy === 'weight') {
+			return a.weight - b.weight;
+		} else if(sortedBy === 'name') {
 				const nameA = a.name.toUpperCase();
 				const nameB = b.name.toUpperCase();
 				if( nameA < nameB) {
@@ -23,30 +36,8 @@ function App() {
 				} else {
 					return 0;
 				}
-			}));
-		} else {
-			setHogsShown(hogsShown => hogs);
 		}
-		setSortedByName(sortedByName => !sortedByName);
-	}
-	
-	function sortWeight() {
-		if(sortedByWeight === false) {
-			setHogsShown(hogsShown => hogsShown.sort((a, b) => a.weight - b.weight));
-		} else {
-			setHogsShown(hogsShown => hogs);
-		}
-		setSortedByWeight(sortedByWeight => !sortedByWeight);
-	}
-
-	function toggleGreased() {
-		if(showGreased === false) {
-			setHogsShown(hogsShown => hogsShown.filter(hog => hog.greased));
-		} else {
-			setHogsShown(hogsShown => hogs);
-		}
-		setShowGreased(showGreased => !showGreased);
-	}
+})
 
 	return (
 		<div className="App">
